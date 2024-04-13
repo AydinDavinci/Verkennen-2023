d7423ecf1164f0de2ef5978a70119120ae7c16e8
Avr6g <143701285+AydinDavinci@users.noreply.github.com>
2024-04-13 19:31:10 +0200

    commit

--- module_3/deel_3/examples/snippet_a/script.js
+++ module_3/deel_3/examples/snippet_a/script.js
@@ -1,14 +1,19 @@
 let playground = document.getElementById('playground');
 
 function proccess_button_click() {
+    console.log('Je hebt iets verwijderd!')
     this.remove();
 }
 
 let button = document.createElement('button');
 button.type = 'button';
-button.innerHTML = 'Remove me';
+button.innerHTML = 'Ik ben niet te verwijderen';
 button.classList.add('btn');
 button.classList.add('btn-primary');
 button.onclick = proccess_button_click;
 
 playground.appendChild(button);
+
+let htmlBtn = document.getElementById('htmlKnop'); 
+htmlBtn.innerHTML = 'Ik ben niet te verwijderen' 
+htmlBtn.onclick = proccess_button_click
--- module_3/deel_3/exercises/cookie_jar/script.js
+++ module_3/deel_3/exercises/cookie_jar/script.js
@@ -1,16 +1,32 @@
 let container = document.getElementById('container');
 
-function create_button(type){
+function create_button(SoortKnop){
     // creeer een standaard button element
-
+    const knop = document.createElement('button');
+    knop.type = 'button';
+    knop.innerHTML = 'knop'
+    knop.classList.add('btn');
+    knop.classList.add('btn-primary');
+    
     // maak een switch of if statement die de specifieke elementen van een button zet
-
+    if (SoortKnop === "add"){
+        knop.innerHTML = 'Voeg toe'
+        knop.onclick = proccess_addcookies_click;
+    }else if (SoortKnop === "take"){
+        knop.innerHTML = 'Pak koekje'
+        knop.onclick = proccess_takecookie_click;
+    }else{
+        knop.innerHTML = 'Sluit pot'
+        knop.classList.add('btn-secondary')
+        knop.onlclick = proccess_placelit_click;
+    }
     // return het button element
+    return knop;
 }
 
 function proccess_takecookie_click(){ 
     let jar; //vul deze variable met het element cookie-jar
-
+    console.log('test')
     // maak een if statement die kijkt hoeveel cookies er nog zijn
     // bij 1 cookie wordt de vraag gesteld: 'Weet u het zeker om de laatste te pakken?'
     // bij 0 wordt de melding gegeven: 'Geen cookies meer' 
@@ -21,7 +37,7 @@ function proccess_takecookie_click(){
 function proccess_addcookies_click() {
     let jar; //vul deze variable met het element cookie-jar
     let currentcookies; // vul deze variablelen met het huidige aantal cookies in de jar 
-
+    console.log('test')
     let repeat = true;
     while (repeat){
         // vraag hoeveel cookies iemand wil toevoegen
@@ -40,7 +56,7 @@ function proccess_addcookies_click() {
 
 function proccess_placelit_click() {
     // verwijder de controls
-
+    console.log('test')
     let lit;  //vul deze variable met het element jar-lit
     
     // voeg de class closed toe aan de lit
@@ -64,13 +80,20 @@ function create_cookie(){
 
 function build_controlls() {
     // maak een array met de 3 verschillende buttons
-    
+    const controlList = [create_button('add'), create_button('take'), create_button('place')];
+    console.log(controlList)
     // genereer een div element met het id controls
-
+    const controldiv = document.createElement("div");
+    controldiv.id = 'controls';
     // loop door de array met buttons en roep voor iedere button de create_button functie aan
+    controlList.forEach(i => {
+        console.log(i);
+        controldiv.appendChild(i)
+    });
     // voeg vervolgens na het aanroepen vann de functie de button aan de controls toe
 
     // voeg de controls toe aan de container
+    container.appendChild(controldiv)
 }
 
 build_controlls()
